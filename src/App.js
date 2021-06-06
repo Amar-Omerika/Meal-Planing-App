3f2e666b312aafecfdfb3c7e090f74e050b46288
Amar Omerika <amar.omerika@edu.fit.ba>
2021-06-06 13:16:50 +0200

    meal-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from "react";
+import MealList from "./MealList";
 import "./App.css";
 
 const App = () => {
 	const [mealData, setMealData] = useState(null);
 	const [calories, setCalories] = useState(2000);
+	const API_KEY = " 5d18562865c34d779598cfa09921d69f";
 	useEffect(() => {
-		setMealData();
+		getMealData();
 	}, []);
 
-	const setMealData = async () => {};
+	const getMealData = async () => {
+		const response = await fetch(
+			"https://api.spoonacular.com/recipes/716429/information?includeNutrition=false"
+		);
+		const data = response.json();
+		console.log(data);
+	};
+	function handleChange() {}
 	return (
 		<div className="App">
 			<section className="controls">
 				<input
 					type="number"
 					placeholder="calories"
-					onChange={setMealData}
+					onChange={handleChange}
 				></input>
 			</section>
+			<button onClick={getMealData}>Get Daily Meal Plan</button>
 		</div>
 	);
 };
