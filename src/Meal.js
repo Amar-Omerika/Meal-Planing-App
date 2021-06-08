11719677d6882405660b63dbdc36df989153d329
Amar Omerika <amar.omerika@edu.fit.ba>
2021-06-08 12:27:02 +0200

    meal-app

--- src/Meal.js
+++ src/Meal.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 const Meal = ({ meal }) => {
-	return <div>Meal</div>;
+	const [imageuRL, setImageURL] = useState("");
+	return (
+		<article>
+			<h1>{meal.title}</h1>
+			<img src={imageuRL} />
+			<ul>
+				<li>Preparation time: {meal.readyInMinutes} minutes</li>
+				<li>Number of servings : {meal.servings}</li>
+			</ul>
+			<a href={meal.sourceUrl}>Visit the Recipe</a>
+		</article>
+	);
 };
 export default Meal;
