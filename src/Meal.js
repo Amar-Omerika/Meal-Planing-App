e1c6471a5a4437f2e753f3209005c61eb29b20c7
Amar Omerika <amar.omerika@edu.fit.ba>
2021-06-09 23:14:51 +0200

    meal-app

--- src/Meal.js
+++ src/Meal.js
@@ -18,7 +18,7 @@ const Meal = ({ meal }) => {
 		<article>
 			<h1>{meal.title}</h1>
 			<img src={imageuRL} />
-			<ul>
+			<ul className="instructions">
 				<li>Preparation time: {meal.readyInMinutes} minutes</li>
 				<li>Number of servings : {meal.servings}</li>
 			</ul>
